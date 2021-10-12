3d0bcb17b1449c52a67d06ee4519c7bc321debfa
CurryCon <chriskeist@gmail.com>
2021-10-12 19:46:49 +0200

    Create MainFunction.py
    a

---
+++ MainFunction.py
@@ -0,0 +1,21 @@
+print("Hello")
+
+name = input("your name ")
+age = input("your age " )
+hobby = input("your hobby ")
+
+print("Hello " + name + ", you are " + age + " and you like " + hobby)
+
+
+    
+Male = input("Are you a male? True/False: ")
+if Male == "True":
+    print("You are a male")
+else:
+    print("You are not a Male")
+
+Tall = input("Are you a True? True/False: ")
+if Tall == "True":
+    print("You are a Tall")
+else:
+    print("You are not a Tall")
