cd110e7049388bccfe07101530fa2030a549ff81
CurryCon <chriskeist@gmail.com>
2021-10-13 18:41:14 +0200

    Update MainFunction.py

--- MainFunction.py
+++ MainFunction.py
@@ -5,16 +5,20 @@ age = input("your age " )
 hobby = input("your hobby ")
 print("Hello " + name + ", you are " + age + " and you like " + hobby)
 
-Male = 1
-Male = input("Are you a male? True/False: ")
-while Male == "False":
-    input("Are you a male? True/False: ")
+
+userInput = ""#
+male = 9
+userInput = input("Are you a male? 1=True/2=False: ")
+while userInput != "3":
+   userInput = input("Are you a male? 1=True/2=False: ")
+   if userInput == "2":
+    print("Are you a male? True/False: ")
+   elif userInput == "1": 
+     print("You are a male")
+     break
 
 
 
-
-if Male == "True":  
-    print("You are a male")
 
 
 
