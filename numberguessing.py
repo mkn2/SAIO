8a95c5d62bafc86a68210400370d0909d4ed750b
CurryCon <chriskeist@gmail.com>
2021-10-15 22:07:27 +0200

    Create numberguessing.py

---
+++ numberguessing.py
@@ -0,0 +1,22 @@
+
+
+import random
+n = random.randint(0,10)
+
+
+Number = n
+
+userInput = ""
+
+userInput = int(input("Input a random number between 0 and 10: "))
+while userInput != n:
+   userInput = int(input("Input a random number between 0 and 10: "))
+   if userInput != n:
+    print("Wrong!")
+   elif userInput == n: 
+     print("Congrats!")
+     break
+   if userInput >= n:
+      print("Lower")
+   elif userInput <= n:
+      print("Higher")
